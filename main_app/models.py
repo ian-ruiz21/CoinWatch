dc5f7f0713b44db488e2538add3b114a492d2c9c
ian-ruiz21 <ruiz.ian21@gmail.com>
2024-10-09 09:49:23 -0500

    Add Coin Model

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,16 @@ from django.contrib.auth.models import User
 # class WatchList(models.Model):
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+
+class Coin(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    price = models.FloatField()
+    market_cap = models.FloatField()
+    volume = models.FloatField()
+    circulating_supply = models.FloatField()
+    change = models.FloatField()
+    image = models.URLField()
+    
+    def __str__(self):
+        return self.name
